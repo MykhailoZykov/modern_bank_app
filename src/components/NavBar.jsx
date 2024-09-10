7d9965c1bda592caed5aa3d4ce320fcd565e9eaa
Mykhailo Zykov <mykhailozykov@gmail.com>
2024-09-10 08:20:28 +0300

    mobile menu fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,7 +34,7 @@ const NavBar = () => {
         <div
           className={`${
             toggle ? "flex" : "hidden"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 bg-discount-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className="list-none flex flex-col justify-end items-center flex-1">
             {navLinks.map((nav, index) => (
